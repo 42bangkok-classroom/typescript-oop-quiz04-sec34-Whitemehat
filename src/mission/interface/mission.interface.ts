3d6553e4e5cc1f1469205478f4120feec28fe005
Whitemehat <wong1234.pe@gmail.com>
2026-03-22 03:19:24 +0700

    practice

---
+++ src/mission/interface/mission.interface.ts
@@ -0,0 +1,8 @@
+export interface IMission  {
+    id: string
+    codename: string
+    status: string
+    startDate: string
+    endDate: string
+    durationDays: number
+}
--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MissionService } from './mission.service';
 
 @Controller('missions')
 export class MissionController {
   constructor(private readonly missionService: MissionService) {}
 
+  @Get()
+  findAll(){
+    return this.missionService.findAll();
+  }
+
   @Get('summary')
   getSummary() {
     return this.missionService.getSummary();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id') id:string,
+    @Query('clearance') clearance: string = 'STANDARD'
+  ){
+    return this.missionService.findOne(id , clearance);
   }
 }
